e99723b60c4de98f7a824c6f3f19f65ba6f3be5c
Sonja Silvonen <sonja.silvonen@outlook.com>
2023-11-12 17:43:40 +0200

    tehtävä 12

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -2,28 +2,38 @@ import requests
 import operator
 from player import Player
 
-def main():
-    url = "https://studies.cs.helsinki.fi/nhlstats/2022-23/players"
-    response = requests.get(url).json()
+class PlayerReader:
+    def __init__(self, url):
+        self.url = url
+        self.response = requests.get(self.url).json()
 
-    #print("JSON-muotoinen vastaus:")
-    #print(response)
-
-    print("Players from FIN:\n")    
-
-    players = []
-
-    for player_dict in response:
-        if player_dict['nationality'] == "FIN":
+    def create_players(self):
+        players = []
+        for player_dict in self.response:
             player = Player(player_dict)
             players.append(player)
+        return players
 
-    #print("Oliot:")
-    
-    players.sort(key=operator.attrgetter('points'), reverse=True)
+class PlayerStats:
+    def __init__(self, reader):
+        self.reader = reader
 
+    def top_scorers_by_nationality(self, nat):
+        players_by_nat = []
+        for player in self.reader:
+            if player.nationality == nat:
+                players_by_nat.append(player)
+        players_by_nat.sort(key=operator.attrgetter('points'), reverse=True)
+        return players_by_nat
+
+def main():
+    url = "https://studies.cs.helsinki.fi/nhlstats/2022-23/players"
+    reader = PlayerReader(url).create_players()
+    stats = PlayerStats(reader)
+    players = stats.top_scorers_by_nationality("FIN")
     for player in players:
         print(player)
+
 
 
 if __name__ == "__main__":
